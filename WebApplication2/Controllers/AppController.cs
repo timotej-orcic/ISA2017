4eb6d936d563966ea02760f57adbd989a3dfc8f8
Tima <Tima@TIMA-PC>
2017-12-28 19:12:03 +0100

    Ugradjen angular; provera spoja Klijent-Sever

---
+++ WebApplication2/Controllers/AppController.cs
@@ -0,0 +1,31 @@
+﻿using Isa2017Cinema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace WebApplication2.Controllers
+{
+    public class AppController : Controller
+    {
+        // GET: App
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: App/Users
+        public JsonResult getUsers()
+        {
+            List<ApplicationUser> retVal = new List<ApplicationUser>();
+
+            ApplicationDbContext dbCtx = ApplicationDbContext.Create();
+
+            retVal = dbCtx.Users.ToList();
+
+            return Json(retVal);
+        }
+    }
+}
