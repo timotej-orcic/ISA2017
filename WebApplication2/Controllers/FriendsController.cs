b39721cd5bc8592c67f6c086413a0417fc1d283e
tttamara23 <tamara.perlinac@yahoo.com>
2018-02-01 23:47:18 +0100

    FriendList, FriendRequest

--- WebApplication2/Controllers/FriendsController.cs
+++ WebApplication2/Controllers/FriendsController.cs
@@ -13,7 +13,9 @@ namespace WebApplication2.Controllers
 {
     public class FriendsController : Controller
     {
-        public List<ApplicationUser> usersToShow;
+
+        public List<ApplicationUser>  usersToShow ;
+        
         // GET: Friends
         public ActionResult Index()
         {
@@ -33,27 +35,31 @@ namespace WebApplication2.Controllers
             ApplicationDbContext ctx = ApplicationDbContext.Create();
             // var user = (RegUser)ctx.Users.Find(User.Identity.GetUserId());
             String id = User.Identity.GetUserId();
-            List<ApplicationUser> friends = new List<ApplicationUser>();
+            //List<ApplicationUser> friends = new List<ApplicationUser>();
             
            
             //RegUser user = (RegUser)ctx.Users.FirstOrDefault(x => x.Id.Equals(id));
             var users = ctx.Users.ToList();
-            
             usersToShow = new List<ApplicationUser>();
+
             var user = await ctx.Users.Include(x => x.FriendList).FirstOrDefaultAsync(x => x.Id == id);
-        
+            
+
             foreach (ApplicationUser regUser in users)
             {
                 if (model.FirstName != null && regUser.Name.ToLower().Equals(model.FirstName.ToLower()) && model.LastName != null && regUser.LastName.ToLower().Equals(model.LastName.ToLower()))
                 {
+                    
                     usersToShow.Add(regUser);
                 }
                 else if (model.FirstName != null && regUser.Name.ToLower().Equals(model.FirstName.ToLower()))
                 {
+                    
                     usersToShow.Add(regUser);
                 }
                 else if (model.LastName != null && regUser.LastName.ToLower().Equals(model.LastName.ToLower()))
                 {
+                   
                     usersToShow.Add(regUser);
                 }
                 else if (model.FirstName == null && model.LastName == null)
@@ -62,11 +68,165 @@ namespace WebApplication2.Controllers
                     return View("ShowFriends");
                 }
             }
-           // user.FriendList.Add(usersToShow[0]);
+            List<ApplicationUser> pendinglist = new List<ApplicationUser>();
+            foreach(ApplicationUser appuser in usersToShow)
+            {
+                var userreq = await ctx.Users.Include(x => x.RequestsList).FirstOrDefaultAsync(x => x.Id == appuser.Id);
+                foreach(Request req in userreq.RequestsList)
+                {
+                    if (req.Sender.Equals(user))
+                    {
+                        pendinglist.Add(appuser);
+                        break;
+                    }
+                }
+            }
+            //user.FriendList.Add(usersToShow[0]);
             ctx.SaveChanges();
+            ViewBag.pendinglist = pendinglist;
             ViewBag.usersToShow = usersToShow;
             ViewBag.FriendList = user.FriendList;
-            return View("ShowFriends");
+            return View("ShowFriends",model);
+        }
+        public async Task<ActionResult> AddFriend(String username, String name, String lastname)
+        {
+            ApplicationDbContext ctx = ApplicationDbContext.Create();
+            String id = User.Identity.GetUserId();
+           // var user = await ctx.Users.Include(x => x.FriendList).FirstOrDefaultAsync(x => x.Id == id);
+            Console.WriteLine(username);
+            ApplicationUser us = ctx.Users.Find(User.Identity.GetUserId());
+            var users = ctx.Users.ToList();
+            ApplicationUser toAdd = new ApplicationUser();
+            var loggeduser = await ctx.Users.Include(x => x.FriendList).FirstOrDefaultAsync(x => x.Id == id);
+            foreach (ApplicationUser u in users)
+            {
+                if (u.UserName.Equals(username))
+                {
+                    
+                    var user = await ctx.Users.Include(x => x.RequestsList).FirstOrDefaultAsync(x => x.UserName == username);
+                    Request friendRequest = new Models.Request()
+                    {
+                        Id = Guid.NewGuid(),
+                        Receiver = user,
+                        Sender = us
+                    };
+                    user.RequestsList.Add(friendRequest);
+                }
+            }
+            ctx.SaveChanges();
+            usersToShow = new List<ApplicationUser>();
+            FriendsViewModel model = new FriendsViewModel
+            {
+                FirstName = name,
+                LastName = lastname
+            };
+            
+            foreach (ApplicationUser regUser in users)
+            {
+                if (name != null && regUser.Name.ToLower().Equals(name.ToLower()) && lastname != null && regUser.LastName.ToLower().Equals(lastname.ToLower()))
+                {
+
+                    usersToShow.Add(regUser);
+                }
+                else if (name != null && regUser.Name.ToLower().Equals(name.ToLower()))
+                {
+
+                    usersToShow.Add(regUser);
+                }
+                else if (lastname != null && regUser.LastName.ToLower().Equals(lastname.ToLower()))
+                {
+
+                    usersToShow.Add(regUser);
+                }
+                else if (name == null && lastname == null)
+                {
+                    ModelState.AddModelError("", "You didn't type nothing.");
+                    return View("ShowFriends");
+                }
+            }
+            List<ApplicationUser> pendinglist = new List<ApplicationUser>();
+            foreach (ApplicationUser appuser in usersToShow)
+            {
+                var userreq = await ctx.Users.Include(x => x.RequestsList).FirstOrDefaultAsync(x => x.Id == appuser.Id);
+                foreach (Request req in userreq.RequestsList)
+                {
+                    if (req.Sender.Equals(loggeduser))
+                    {
+                        pendinglist.Add(appuser);
+                        break;
+                    }
+                }
+            }
+            ViewBag.pendinglist = pendinglist;
+            ViewBag.usersToShow = usersToShow;
+            ViewBag.FriendList = us.FriendList;
+            return View("ShowFriends", model);
+        }
+
+        public async Task<ActionResult> RemoveFriend(String username, String name, String lastname)
+        {
+            ApplicationDbContext ctx = ApplicationDbContext.Create();
+            String id = User.Identity.GetUserId();
+            var user = await ctx.Users.Include(x => x.FriendList).FirstOrDefaultAsync(x => x.Id == id);
+            Console.WriteLine(username);
+            var users = ctx.Users.ToList();
+            ApplicationUser forRemove = new ApplicationUser();
+            foreach(ApplicationUser friend in user.FriendList)
+            {
+                if (friend.UserName.Equals(username))
+                {
+                    forRemove = friend;
+                   
+                }
+            }
+            usersToShow = new List<ApplicationUser>();
+            user.FriendList.Remove(forRemove);
+            ctx.SaveChanges();
+            foreach (ApplicationUser regUser in users)
+            {
+                if (name != null && regUser.Name.ToLower().Equals(name.ToLower()) && lastname != null && regUser.LastName.ToLower().Equals(lastname.ToLower()))
+                {
+
+                    usersToShow.Add(regUser);
+                }
+                else if (name != null && regUser.Name.ToLower().Equals(name.ToLower()))
+                {
+
+                    usersToShow.Add(regUser);
+                }
+                else if (lastname != null && regUser.LastName.ToLower().Equals(lastname.ToLower()))
+                {
+
+                    usersToShow.Add(regUser);
+                }
+                else if (name == null && lastname == null)
+                {
+                    ModelState.AddModelError("", "You didn't type nothing.");
+                    return View("ShowFriends");
+                }
+            }
+            FriendsViewModel model = new FriendsViewModel
+            {
+                FirstName = name,
+                LastName = lastname
+            };
+            List<ApplicationUser> pendinglist = new List<ApplicationUser>();
+            foreach (ApplicationUser appuser in usersToShow)
+            {
+                var userreq = await ctx.Users.Include(x => x.RequestsList).FirstOrDefaultAsync(x => x.Id == appuser.Id);
+                foreach (Request req in userreq.RequestsList)
+                {
+                    if (req.Sender.Equals(user))
+                    {
+                        pendinglist.Add(appuser);
+                        break;
+                    }
+                }
+            }
+            ViewBag.pendinglist = pendinglist;
+            ViewBag.usersToShow = usersToShow;
+            ViewBag.FriendList = user.FriendList;
+            return View("ShowFriends",model);
         }
     }
 }
--- WebApplication2/Models/IdentityModels.cs
+++ WebApplication2/Models/IdentityModels.cs
@@ -5,12 +5,14 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
+using WebApplication2.Models;
 
 namespace Isa2017Cinema.Models
 {
 
     public enum Type { GOLD, SILVER, BRONZE, DEFAULT }
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    
     public class ApplicationUser : IdentityUser
     {
         public String Name { get; set; }
@@ -20,10 +22,11 @@ namespace Isa2017Cinema.Models
         public Double Points { get; set; }
         public Type UserType { get; set; }
         public List<ApplicationUser> FriendList { get; set; }
-        public List<ApplicationUser> RequestsList { get; set; }
+        public List<ApplicationUser> RequestsUserNames { get; set; }
         public List<Ticket> ReservationsList { get; set; }
         public List<Recension> RecensionList { get; set; }
         public List<Post> PostsList { get; set; }
+        public List<Request> RequestsList { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
