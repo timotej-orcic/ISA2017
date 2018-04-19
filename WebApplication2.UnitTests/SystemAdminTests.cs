1145bb842613836a6ac239a0b41d3d7798b6f324
timotej-orcic <timotej.orcic@gmail.com>
2018-04-19 15:49:38 +0200

    Nastavak rada na testovima

--- WebApplication2.UnitTests/SystemAdminTests.cs
+++ WebApplication2.UnitTests/SystemAdminTests.cs
@@ -12,6 +12,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Threading.Tasks;
 
 namespace WebApplication2.UnitTests
 {
@@ -21,17 +22,23 @@ namespace WebApplication2.UnitTests
         [Test]
         public void AddAdmin_RequestNotAuthenticated_RedirectToLoginPage()
         {
-            var request = new Mock<HttpRequestBase>();
-            request.SetupGet(x => x.IsAuthenticated).Returns(false);
-            var context = new Mock<HttpContextBase>();
-            context.SetupGet(x => x.Request).Returns(request.Object);
+            //Arrange
             System_AdminController sac = new System_AdminController();
-            sac.ControllerContext = new ControllerContext(context.Object, new RouteData(), sac);
+
+            var requestMock = new Mock<HttpRequestBase>();
+            requestMock.SetupGet(x => x.IsAuthenticated).Returns(false);
+
+            var contextMock = new Mock<HttpContextBase>();
+            contextMock.SetupGet(x => x.Request).Returns(requestMock.Object);
+
+            sac.ControllerContext = new ControllerContext(contextMock.Object, new RouteData(), sac);
             var inputVM = new AddNewAdminViewModel();
 
+            //Act
             var result = sac.AddAdmin(inputVM).Result;
             RedirectToRouteResult routeResult = result as RedirectToRouteResult;
 
+            //Assers
             Assert.That(routeResult, Is.Not.Null);
             Assert.AreEqual(routeResult.RouteValues["action"], "Login");
             Assert.That(sac.TempData.ContainsKey("success"), Is.False);
@@ -40,51 +47,159 @@ namespace WebApplication2.UnitTests
         [Test]
         public void AddAdmin_UserNotInGoodRole_RedirectToHomePage()
         {
-            var request = new Mock<HttpRequestBase>();
-            request.SetupGet(x => x.IsAuthenticated).Returns(true);
-            var context = new Mock<HttpContextBase>();
-            context.SetupGet(x => x.Request).Returns(request.Object);
-            var userStore = new Mock<IUserStore<ApplicationUser>>();
-            var userManager = new Mock<UserManager<ApplicationUser>>(userStore.Object);
-            var user = new ApplicationUser { Id = "1", UserName = "User" };
-            userManager.Object.Create(user);
-            userManager.Object.AddToRole(user.Id, "Regular_User");
-
-            //InitUserRoles();
-            context.Setup(ctx => ctx.User).Returns((IPrincipal)user);
-
+            //Arrange
             System_AdminController sac = new System_AdminController();
-            sac.ControllerContext = new ControllerContext(context.Object, new RouteData(), sac);
+
+            var userMock = new Mock<IPrincipal>();
+            userMock.Setup(p => p.IsInRole("Regular_User")).Returns(true);
+
+            var requestMock = new Mock<HttpRequestBase>();
+            requestMock.SetupGet(x => x.IsAuthenticated).Returns(true);
+
+            var contextMock = new Mock<HttpContextBase>();
+            contextMock.SetupGet(ctx => ctx.User).Returns(userMock.Object);
+            contextMock.SetupGet(ctx => ctx.Request).Returns(requestMock.Object);
+
+            var controllerContextMock = new Mock<ControllerContext>();
+            controllerContextMock.SetupGet(con => con.HttpContext).Returns(contextMock.Object);
+
+            sac.ControllerContext = controllerContextMock.Object;
             var inputVM = new AddNewAdminViewModel();
 
+            //Act
             var result = sac.AddAdmin(inputVM).Result;
             RedirectToRouteResult routeResult = result as RedirectToRouteResult;
 
+            //Assert
             Assert.That(routeResult, Is.Not.Null);
-            Assert.AreEqual(routeResult.RouteValues["action"], "Login");
+            Assert.AreEqual(routeResult.RouteValues["action"], "Index");
             Assert.That(sac.TempData.ContainsKey("success"), Is.False);
         }
 
-        public void InitUserRoles()
+        [Test]
+        public void AddAdmin_AddLocationAdminAndSomeVMAttributesAreNull_RedirectToAddNewAdminPage()
         {
-            List<string> Roles = new List<string>
+            //Arrange
+            System_AdminController sac = new System_AdminController();
+
+            var userMock = new Mock<IPrincipal>();
+            userMock.Setup(p => p.IsInRole("System_Admin")).Returns(true);
+
+            var requestMock = new Mock<HttpRequestBase>();
+            requestMock.SetupGet(x => x.IsAuthenticated).Returns(true);
+
+            var contextMock = new Mock<HttpContextBase>();
+            contextMock.SetupGet(ctx => ctx.User).Returns(userMock.Object);
+            contextMock.SetupGet(ctx => ctx.Request).Returns(requestMock.Object);
+
+            var controllerContextMock = new Mock<ControllerContext>();
+            controllerContextMock.SetupGet(con => con.HttpContext).Returns(contextMock.Object);
+
+            sac.ControllerContext = controllerContextMock.Object;
+            var inputVM = new AddNewAdminViewModel()
             {
-                "Regular_User",
-                "System_Admin",
-                "Fanzone_Admin",
-                "Location_Admin"
+                Admin_Type = AdminType.LOCATION_ADMIN,
+                Email = "",
+                Name = null,
+                LastName = "",
+                UserName = null,
+                MyLocationId = ""
             };
 
-            var roleStore = new Mock<IRoleStore<IdentityRole>>();
-            var roleManager = new Mock<RoleManager<IdentityRole>>(roleStore.Object, null, null, null, null);
+            //Act
+            var result = sac.AddAdmin(inputVM).Result;
+            RedirectToRouteResult routeResult = result as RedirectToRouteResult;
 
-            foreach (var role in Roles)
+            //Assert
+            Assert.That(routeResult, Is.Not.Null);
+            Assert.AreEqual(routeResult.RouteValues["action"], "AddNewAdmin");
+            Assert.AreEqual(sac.ModelState.Values.Count, 1);
+            Assert.AreEqual(sac.ModelState.Values.ElementAt(0).Errors.ElementAt(0).ErrorMessage, "Error while trying to add new admin (some fields are null)");
+        }
+
+        [Test]
+        public void AddAdmin_AddLocationAdminAndLocIdIsNull_RedirectToAddNewAdminPage()
+        {
+            //Arrange
+            System_AdminController sac = new System_AdminController();
+
+            var userMock = new Mock<IPrincipal>();
+            userMock.Setup(p => p.IsInRole("System_Admin")).Returns(true);
+
+            var requestMock = new Mock<HttpRequestBase>();
+            requestMock.SetupGet(x => x.IsAuthenticated).Returns(true);
+
+            var contextMock = new Mock<HttpContextBase>();
+            contextMock.SetupGet(ctx => ctx.User).Returns(userMock.Object);
+            contextMock.SetupGet(ctx => ctx.Request).Returns(requestMock.Object);
+
+            var controllerContextMock = new Mock<ControllerContext>();
+            controllerContextMock.SetupGet(con => con.HttpContext).Returns(contextMock.Object);
+
+            sac.ControllerContext = controllerContextMock.Object;
+            var inputVM = new AddNewAdminViewModel()
             {
-                if (!roleManager.Object.RoleExists(role))
-                {
-                    var roleResult = roleManager.Object.Create(new IdentityRole(role));
-                }
-            }
+                Admin_Type = AdminType.LOCATION_ADMIN,
+                Email = "",
+                Name = "",
+                LastName = "",
+                UserName = "",
+                MyLocationId = null
+            };
+
+            //Act
+            var result = sac.AddAdmin(inputVM).Result;
+            RedirectToRouteResult routeResult = result as RedirectToRouteResult;
+
+            //Assert
+            Assert.That(routeResult, Is.Not.Null);
+            Assert.AreEqual(routeResult.RouteValues["action"], "AddNewAdmin");
+            Assert.AreEqual(sac.ModelState.Values.Count, 1);
+            Assert.AreEqual(sac.ModelState.Values.ElementAt(0).Errors.ElementAt(0).ErrorMessage, "Error: Admin location is null.");
+        }
+
+
+        //ovaj vec mora biti integration zbog pristupa bazi
+        [Test]
+        public async Task AddAdmin_AddSystemAdminAlreadyExist_RedirectToAddNewAdminPage()
+        {
+            //Arrange
+            System_AdminController sac = new System_AdminController();
+
+            var userMock = new Mock<IPrincipal>();
+            userMock.Setup(p => p.IsInRole("System_Admin")).Returns(true);
+
+            var requestMock = new Mock<HttpRequestBase>();
+            requestMock.SetupGet(x => x.IsAuthenticated).Returns(true);
+
+            var contextMock = new Mock<HttpContextBase>();
+            contextMock.SetupGet(ctx => ctx.User).Returns(userMock.Object);
+            contextMock.SetupGet(ctx => ctx.Request).Returns(requestMock.Object);
+
+            var controllerContextMock = new Mock<ControllerContext>();
+            controllerContextMock.SetupGet(con => con.HttpContext).Returns(contextMock.Object);
+
+            sac.ControllerContext = controllerContextMock.Object;
+            var inputVM = new AddNewAdminViewModel()
+            {
+                Admin_Type = AdminType.LOCATION_ADMIN,
+                Email = "main.admin@isa.com",
+                Name = "",
+                LastName = "",
+                UserName = "",
+                MyLocationId = ""
+            };
+
+            //Act
+            var result = await sac.AddAdmin(inputVM);
+            RedirectToRouteResult routeResult = result as RedirectToRouteResult;
+
+            //Assert
+            Assert.That(routeResult, Is.Not.Null);
+            Assert.AreEqual(routeResult.RouteValues["action"], "AddNewAdmin");
+            Assert.AreEqual(sac.ModelState.Values.Count, 1);
+            string errMsg = sac.ModelState.Values.ElementAt(0).Errors.ElementAt(0).ErrorMessage;
+            Assert.AreEqual(errMsg, "User with this email adress already exists");
         }
 
     }
